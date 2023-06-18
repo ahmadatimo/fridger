b03430e67f65ff0fbbcecc9a384333306e5566e3
Ahmed Hatem <ahmadatimo2002@gmail.com>
2023-06-18 21:58:47 +0300

    edit
    changed files name that's it

--- app/src/main/java/com/example/fridger/Fridge.java
+++ app/src/main/java/com/example/fridger/Fridge.java
@@ -47,6 +47,14 @@ public class Fridge extends AppCompatActivity {
         fridge = findViewById(R.id.FfridgeIcon);
         recipe = findViewById(R.id.FrecipeIcon);
         shopping = findViewById(R.id.FshoppingIcon);
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
 
--- app/src/main/java/com/example/fridger/Recipes.java
+++ app/src/main/java/com/example/fridger/Recipes.java
@@ -53,6 +53,14 @@ public class Recipes extends AppCompatActivity {
         shopping = findViewById(R.id.RshoppingIcon);
         button = findViewById(R.id.howTo);
         title = findViewById(R.id.recipeTitle);
+        View decorView = getWindow().getDecorView();
+// Hide both the navigation bar and the status bar.
+// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+// a general rule, you should design your app to hide the status bar whenever you
+// hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
     public void initializingButtons()
